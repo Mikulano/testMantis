b81eb58c8001eb7212647a26df2c2cd7b7fa6c91
Your Name <you@example.com>
2017-06-02 00:01:50 +0300

    pipeline

---
+++ pipeline.groovy
@@ -0,0 +1,15 @@
+node {
+ 
+  	git url: 'https://github.com/sergusha/bddmantis.git', branch: 'master'
+  	sh "mvn clean test"
+  	step([$class: 'CucumberReportPublisher',
+        	failedFeaturesNumber: 0,
+        	failedScenariosNumber: 0,
+        	failedStepsNumber: 0, fileExcludePattern: '',
+        	fileIncludePattern: '**/mainReport.json',
+        	jsonReportDirectory: '',
+        	pendingStepsNumber: 0,
+        	skippedStepsNumber: 0,
+        	undefinedStepsNumber: 0])
+}
+
